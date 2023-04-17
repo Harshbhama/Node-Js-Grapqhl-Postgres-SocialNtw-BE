88050aafb2f0508e6ae5b1d8b13a41b2a50e345b
Harsh Bhama <harshbhama@Harshs-MacBook-Air.local>
2023-04-17 23:28:15 +0530

    add login

--- node-mongo-api/index.js
+++ node-mongo-api/index.js
@@ -5,6 +5,14 @@ const application = express();
 const path = require("path")
 const bodyparser = require("body-parser")
 const cors = require('cors');
+const redis = require('redis');
+const client = redis.createClient();
+
+client.on('connect', function() {
+    console.log('Connected to redis server');
+}).on('error', function (error) {
+    console.log(error);
+});
 
 const CourseController = require('./controllers/courses');
 const LoginController = require('./controllers/authentication');
@@ -33,7 +41,7 @@ application.use("/authentication", LoginController);
 application.use("/course", CourseController);
 application.use("/products",ProductController);
 application.use("/user", UserController);
-application.listen("4000", () => {
-    console.log("server started");
+// application.listen("4000", () => {
+//     console.log("server started");
 
-})
+// })
